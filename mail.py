97773840cdc359ba0710018bc4b4ebc5d48b5144
sorcer0123f <maes3859@163.com>
2021-03-19 21:03:16 +0800

    c1

---
+++ mail.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "iampalakjain01@gmail.com"  # Enter your address
+receiver_email = "itspalak19@gmail.com"  # Enter receiver address
+password = "xecbeupbulzfwpos"
+message = """\
+Subject: Hi there,
+you have some error on your website
+Please check !
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
