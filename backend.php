725908031221a9d3a34bc4ed6b00ea2ffbbbefdf
sorcer0123f <maes3859@163.com>
2021-04-03 02:07:46 +0800

    c1

--- backend.php
+++ backend.php
@@ -1,8 +1,8 @@
 <?php       
-<h1> Hello! </h1>
+echo '<h1> Hello! </h1>
         <h2> Hope this page finds you good. </h2>
 
-        <h4> Welcome to my Backend page </h4>
+        <h4> Welcome to my Backend page </h4>'
 
 ?>
 
